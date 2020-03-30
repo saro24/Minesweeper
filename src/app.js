060826d8de89a38091a9656f1ef9d0dd597eadea
Sara bouglam <sarabouglam@gmail.com>
2020-03-30 13:21:41 +0100

    initial design

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
-import React,{Component} from 'react'; 
-import  Mines from './mSweeperClass.js';
- 
-class App extends Component { 
- constructor(){ 
-     super(); 
-     this.state={ 
-         n:10
-     }
-    
- }
+import React, { Component } from 'react';
+import Mines from './mSweeperClass.js';
+import  './style.css'; 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            n: 10
+        }
+    }
+    render() {
 
- render(){ 
-      
-     return (
-      <div>
-        <div className="UpperPart"> </div>  
-         <Mines  /> 
-      
-     </div> 
-     )
- }
+        return (
+            <div className="container">
+                <div className="UpperPart"> 
+                  <div className="time">00:22 </div>
+                  <div className="emoji"> <i className="fas fa-laugh-beam"></i> </div>
+                  
+                  <div className="score">0008</div> 
+
+                </div>
+                <Mines />
+
+            </div>
+        )
+    }
 
 }
 
--- src/mines.js
+++ src/mines.js
@@ -1,22 +1,29 @@
-import React,{Component} from 'react'; 
- 
-const  Holes =(props)=>{ 
-    var n=5; 
-    var array=[]; 
-    var i=0 ; 
-    props.mines(n).map((elements)=>  elements.forEach((element) => { array.push(element)}));
+import React, { Component } from 'react';
+
+const Holes = (props) => {
+    var array = [];
+    var subarray2= []; 
+    var container= [];
+    var n=props.n; 
+    props.mines.map((elements) => elements.forEach((element) => { array.push(element) }));
+
     return (
-        <div className="buttons">
-      {
-            array.map((elements) => {
-             return (
-            <button key={elements.pointer}> {elements.pointer} </button>
-             )
-             i++;            
-         })
-     }
- </div>
- )  
+        <div className="holes">
+            {
+                props.mines.map((elements) => {         
+                  return (  
+                     <div key={n--}>
+                      {
+                       elements.map((element) => {
+                        return (<button key={element.pointer}>   </button>)   
+                      })
+                      }
+                      </div>      
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Holes; 
