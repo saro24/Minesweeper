f52ff9e096cc37d5fb11fdcb16efdc5b4bbdfe88
Sara bouglam <sarabouglam@gmail.com>
2020-03-30 16:15:08 +0100

    finishing the mini game design

--- src/app.js
+++ src/app.js
@@ -12,12 +12,20 @@ class App extends Component {
 
         return (
             <div className="container">
-                <div className="UpperPart"> 
-                  <div className="time">00:22 </div>
-                  <div className="emoji"> <i className="fas fa-laugh-beam"></i> </div>
-                  
-                  <div className="score">0008</div> 
-
+               <div className="Topinfo">
+               <i className="fas fa-bomb"></i>
+                <p className="title">Minesweeper</p> 
+                <p className="hide">__</p> 
+                 <p className="delete">X</p>
+                 
+                 </div> 
+                 <div className="info"> <p> Game </p> <p> Help</p> </div>
+                 <div className="UpperPart"> 
+                 <div className="subUpperPart"> 
+                 <div className="time">00:22 </div>
+                 <i className="far fa-laugh-beam"></i>                   
+                 <div className="score">0008</div> 
+                 </div>
                 </div>
                 <Mines />
 
